348b446df487fb6ba851b0d0a1a9a1b38a6d8dd9
takumi-1234 <tkngtkmrwgnnns@gmail.com>
2025-12-28 00:36:39 +0900

    fix(ViewerEditor): remove duplicate re-exports to resolve TS2300

--- src/components/common/ViewerEditor/index.ts
+++ src/components/common/ViewerEditor/index.ts
@@ -2,16 +2,7 @@ export { default as ProblemTextEditor } from './ProblemTextEditor';
 export { default as PreviewBlock } from './PreviewBlock';
 export { default as CodeEditorWrapper } from './CodeEditorWrapper';
 
-export { default as Descriptive } from './Descriptive';
-export { default as Selection } from './Selection';
-export { default as FillInBlank } from './FillInBlank';
-export { default as TrueFalse } from './TrueFalse';
-export { default as MathCalculation } from './MathCalculation';
-export { default as Proof } from './Proof';
-export { default as Programming } from './Programming';
-export { default as CodeReading } from './CodeReading';
-
-// Named per design spec
+// ViewerEditor exports (TypeX_ names per spec)
 export { default as Type1_Descriptive } from './Type1_Descriptive';
 export { default as Type2_Selection } from './Type2_Selection';
 export { default as Type4_FillInBlank } from './Type4_FillInBlank';
@@ -20,5 +11,7 @@ export { default as Type6_MathCalculation } from './Type6_MathCalculation';
 export { default as Type7_Proof } from './Type7_Proof';
 export { default as Type8_Programming } from './Type8_Programming';
 export { default as Type9_CodeReading } from './Type9_CodeReading';
+
+// Common utilities (single export set)
 
 export { default as ProblemAnswerWrapper } from './ProblemAnswerWrapper';
